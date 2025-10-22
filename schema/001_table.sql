720214fad2693e1038a68d600889ab965519f76f
Tolben <vitek.pasek@gmail.com>
2025-10-22 02:21:09 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/001_table.sql
+++ schema/001_table.sql
@@ -1,4 +1,4 @@
--- Auto-generated from schema-map.psd1 @ 1e83bb6 (2025-10-21T10:18:36+02:00)
+-- Auto-generated from schema-map.psd1 (map@1e83bb6)
 -- table: webhook_outbox
 CREATE TABLE IF NOT EXISTS webhook_outbox (
   id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
