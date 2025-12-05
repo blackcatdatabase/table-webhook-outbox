a3838a6efedf2fdf9bf4df5ffbc607b11e6892ba
Vito416 <vito416@protonmail.com>
2025-12-05 22:07:07 +0100

    Fixed deep problems in schema, added safe checks not to repeat in future, using sha1 now in comment

--- schema/040_views_joins.mysql.sql
+++ schema/040_views_joins.mysql.sql
@@ -1,4 +1,4 @@
--- Auto-generated from joins-mysql.yaml (map@85230ed)
+-- Auto-generated from joins-mysql.yaml (map@sha1:DA70105A5B799F72A56FEAB71A5171F946A770D2)
 -- engine: mysql
 -- view:   webhook_outbox_metrics
 
--- schema/040_views_joins.postgres.sql
+++ schema/040_views_joins.postgres.sql
@@ -1,4 +1,4 @@
--- Auto-generated from joins-postgres.yaml (map@85230ed)
+-- Auto-generated from joins-postgres.yaml (map@sha1:29CF395A3A4C8964482083733F8E613ABFBEF5CC)
 -- engine: postgres
 -- view:   webhook_outbox_metrics
 
